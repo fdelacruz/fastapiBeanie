14c73b6941eb0f012fe174b87501566d4d86d80e
Francisco De La Cruz <helloquico@gmail.com>
2024-12-18 11:39:36 +0100

    update app.py to include the startup event

--- app/server/app.py
+++ app/server/app.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 
+from app.server.database import init_db
+from app.server.routes.product_review import router as Router
+
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def start_db():
+    await init_db()
 
 
 @app.get("/", tags=["Root"])
